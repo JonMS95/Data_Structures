2c56634c294ef240b0a2c3a2124e3f221343c69b
JonMS95 <jon_portugalete@hotmail.com>
2022-12-27 17:15:22 +0100

    Added a new node instantiation for testing purposes.

--- Binary_tree/Source_files/main.cpp
+++ Binary_tree/Source_files/main.cpp
@@ -1,4 +1,5 @@
 #include "BinaryTreeNode.hpp"
+#include <memory>
 
 int main()
 {
@@ -6,6 +7,9 @@ int main()
     BinaryTreeNode* node3 = new BinaryTreeNode(3);
     BinaryTreeNode* node2 = new BinaryTreeNode(2, node4, nullptr);
     BinaryTreeNode* node1 = new BinaryTreeNode(1, node2, node3);
+
+    BinaryTreeNode* node5 = new BinaryTreeNode(5);
+    node2->SetRightNode(node5);
 
     return 0;
 }
