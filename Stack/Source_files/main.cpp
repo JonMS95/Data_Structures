30b8fc7b6b6b37050e70b00565f89263c3325dbf
JonMS95 <jon_portugalete@hotmail.com>
2023-01-07 17:34:30 +0100

    Added stack data structure directory.

---
+++ Stack/Source_files/main.cpp
@@ -0,0 +1,11 @@
+// Stack type in C++ is a container type, which allows us to organise some data in a LIFO queue fashion.
+// #include its header file to use it.
+
+#include <iostream>
+#include <stack>
+
+int main()
+{
+
+    return 0;
+}
