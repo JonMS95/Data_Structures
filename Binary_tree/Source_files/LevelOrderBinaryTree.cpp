39b25f74fe16d33ac3fc9b1ec3a923765aa496dc
JonMS95 <jon_portugalete@hotmail.com>
2023-01-22 17:52:38 +0100

    Created LOBT class!

---
+++ Binary_tree/Source_files/LevelOrderBinaryTree.cpp
@@ -0,0 +1,42 @@
+#include "LevelOrderBinaryTree.hpp"
+
+LevelOrderBinaryTree::LevelOrderBinaryTree()
+{
+    this->root = std::make_shared<BinaryTreeNode>();
+}
+
+LevelOrderBinaryTree::LevelOrderBinaryTree(int data)
+{
+    this->root = std::make_shared<BinaryTreeNode>(data);
+}
+
+LevelOrderBinaryTree::~LevelOrderBinaryTree(){}
+
+void LevelOrderBinaryTree::LevelOrderInsert(int data)
+{
+    std::queue<std::shared_ptr<BinaryTreeNode>> q;
+    
+    if(this->root == nullptr)
+    {
+        return;
+    }
+    
+    q.push(this->GetRootValue());
+
+    while(true)
+    {
+        if(q.front()->GetLeftNode() == nullptr)
+        {
+            q.front()->SetLeftNode(std::make_shared<BinaryTreeNode>(data));
+            return;
+        }
+        if(q.front()->GetRightNode() == nullptr)
+        {
+            q.front()->SetRightNode(std::make_shared<BinaryTreeNode>(data));
+            return;
+        }
+        q.push(q.front()->GetLeftNode());
+        q.push(q.front()->GetRightNode());
+        q.pop();
+    }
+}
