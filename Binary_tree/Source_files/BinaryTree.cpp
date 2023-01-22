ae9dd73db93121ef23391c15020bb0ec5fd181ad
JonMS95 <jon_portugalete@hotmail.com>
2023-01-22 16:59:23 +0100

    Created and tested base BT class. It will be used later as a template to build Binary Search Tree and Level Order Binary Tree

---
+++ Binary_tree/Source_files/BinaryTree.cpp
@@ -0,0 +1,228 @@
+#include "BinaryTree.hpp"
+
+BinaryTree::BinaryTree(int data)
+{
+    root = std::make_shared<BinaryTreeNode>(data);
+    std::cout << "Created Binary Tree!\r\n";
+}
+
+BinaryTree::~BinaryTree()
+{
+    std::cout << "Removed Binary Tree!\r\n";
+}
+
+std::shared_ptr<BinaryTreeNode> BinaryTree::GetBTRoot()
+{
+    return this->root;
+}
+
+void BinaryTree::TraverseLevelOrder(void(*function)(void*))
+{
+    if(this->root == nullptr)
+    {
+        return;
+    }
+    std::queue<std::shared_ptr<BinaryTreeNode>> q;
+    q.push(this->root);
+
+    while(q.empty() == false)
+    {
+        if(function != nullptr)
+        {
+            function(q.front().get());
+        }
+        
+        if(q.front()->GetLeftNode() != nullptr)
+        {
+            q.push(q.front()->GetLeftNode());
+        }
+        if(q.front()->GetRightNode() != nullptr)
+        {
+            q.push(q.front()->GetRightNode());
+        }
+        q.pop();
+    }
+}
+
+bool BinaryTree::IsBTComplete()
+{
+    std::vector<bool> nullityVector;
+    bool previousValue = true;
+    std::queue<std::shared_ptr<BinaryTreeNode>> q;
+
+    if(this->root == nullptr)
+    {
+        return false;
+    }
+
+    q.push(this->root);
+    nullityVector.push_back(true);
+
+    while(q.empty() == false)
+    {
+        if(q.front()->GetLeftNode() != nullptr)
+        {
+            q.push(q.front()->GetLeftNode());
+            nullityVector.push_back(true);
+        }
+        else
+        {
+            nullityVector.push_back(false);
+        }
+        if(q.front()->GetRightNode() != nullptr)
+        {
+            q.push(q.front()->GetRightNode());
+            nullityVector.push_back(true);
+        }
+        else
+        {
+            nullityVector.push_back(false);
+        }
+        q.pop();
+    }
+
+    for(long unsigned int i = 0; i < nullityVector.size(); i++)
+    {
+        if((previousValue == false) && (nullityVector[i] == true))
+        {
+            return false;
+        }
+        previousValue = nullityVector[i];
+    }
+
+    return true;
+}
+
+int BinaryTree::GetDepth(std::shared_ptr<BinaryTreeNode> node, int depth = 0, int currentCounter = 0)
+{
+    int dp = depth;
+    int cc = currentCounter;
+
+    if (node == nullptr)
+    {
+        return -1;
+    }
+
+    (cc)++;
+
+    if(cc > dp)
+    {
+        dp = cc;
+    }
+
+    if (node->GetLeftNode() != nullptr)
+    {
+        dp = GetDepth(node->GetLeftNode(), dp, cc);
+    }
+
+    if (node->GetRightNode() != nullptr)
+    {
+        dp = GetDepth(node->GetRightNode(), dp, cc);
+    }
+
+    (cc)--;
+
+    return dp;
+}
+
+int BinaryTree::GetBTDepth()
+{
+    return this->GetDepth(this->root, 0, 0);
+}
+
+void BinaryTree::printNodeData(void* node)
+{
+    BinaryTreeNode* castedNode = (BinaryTreeNode*) node;
+
+    if(castedNode == nullptr)
+    {
+        std::cout << "Null node!\r\n";
+        return;
+    }
+
+    std::cout << castedNode->GetData() << "\r\n";
+}
+
+void BinaryTree::BTRecursiveTraversal(std::shared_ptr<BinaryTreeNode> node,
+                         void (*preOrderFn)(void*),
+                         void (*inOrderFn)(void*),
+                         void (*postOrderFn)(void*)
+                        )
+{
+    if(node == nullptr)
+    {
+        return;
+    }
+
+    if(preOrderFn != nullptr)
+    {
+        preOrderFn(node.get());
+    }
+
+    if(node->GetLeftNode() != nullptr)
+    {
+        BTRecursiveTraversal(node->GetLeftNode(), preOrderFn, inOrderFn, postOrderFn);
+    }
+
+    if(inOrderFn != nullptr)
+    {
+        inOrderFn(node.get());
+    }
+
+    if(node->GetRightNode() != nullptr)
+    {
+        BTRecursiveTraversal(node->GetRightNode(), preOrderFn, inOrderFn, postOrderFn);
+    }
+
+    if(postOrderFn != nullptr)
+    {
+        postOrderFn(node.get());
+    }
+}
+
+void BinaryTree::PrintLevelOrder()
+{
+    this->TraverseLevelOrder(this->printNodeData);
+}
+
+void BinaryTree::PrintPreOrder()
+{
+    this->BTRecursiveTraversal(this->root, this->printNodeData, nullptr, nullptr);
+}
+
+void BinaryTree::PrintInOrder()
+{
+    this->BTRecursiveTraversal(this->root, nullptr, this->printNodeData, nullptr);
+}
+
+void BinaryTree::PrintPostOrder()
+{
+    this->BTRecursiveTraversal(this->root, nullptr, nullptr, this->printNodeData);
+}
+
+void BinaryTree::LevelOrderInsert(int data)
+{
+    std::queue<std::shared_ptr<BinaryTreeNode>> q;
+    q.push(this->root);
+
+    while(true)
+    {
+        if(q.front()->GetLeftNode() == nullptr)
+        {
+            q.front()->SetLeftNode(std::make_shared<BinaryTreeNode>(data));
+            return;
+        }
+        if(q.front()->GetRightNode() == nullptr)
+        {
+            q.front()->SetRightNode(std::make_shared<BinaryTreeNode>(data));
+            return;
+        }
+        q.push(q.front()->GetLeftNode());
+        q.push(q.front()->GetRightNode());
+        q.pop();
+    }
+}
+
+// void LevelOrderInsert(std::shared_ptr<BinaryTreeNode> node, int data);
+
+// int InsertBinarySearchTree(std::shared_ptr<BinaryTreeNode> node, int data);
