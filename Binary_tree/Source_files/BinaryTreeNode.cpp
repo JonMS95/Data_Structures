d5134dac6498036706c15c5b7e91f5b89cfc61e1
JonMS95 <jon_portugalete@hotmail.com>
2023-01-28 12:56:39 +0100

    Added some comments.

--- Binary_tree/Source_files/BinaryTreeNode.cpp
+++ Binary_tree/Source_files/BinaryTreeNode.cpp
@@ -36,31 +36,45 @@ BinaryTreeNode::~BinaryTreeNode()
     std::cout << "Removed node!\r\n";
 }
 
+/// @brief Sets data value for the given node.
+/// @param data value that's meant to be set.
 void BinaryTreeNode::SetData(int data)
 {
     this->data = data;
 }
 
+
+/// @brief Sets pointer to left child node.
+/// @param left Pointer to the node that's going to be set as left child.
 void BinaryTreeNode::SetLeftNode(std::shared_ptr<BinaryTreeNode> left)
 {
     this->left = left;
 }
 
+/// @brief Sets pointer to right child node.
+/// @param left Pointer to the node that's going to be set as right child.
 void BinaryTreeNode::SetRightNode(std::shared_ptr<BinaryTreeNode> right)
 {
     this->right = right;
 }
 
+
+/// @brief Gets the data stored in the current node.
+/// @return Value of the data variable.
 int BinaryTreeNode::GetData()
 {
     return this->data;
 }
 
+/// @brief Left child node getter.
+/// @return Pointer to the left child node.
 std::shared_ptr<BinaryTreeNode> BinaryTreeNode::GetLeftNode()
 {
     return this->left;
 }
 
+/// @brief Right child node getter.
+/// @return Pointer to the right child node.
 std::shared_ptr<BinaryTreeNode> BinaryTreeNode::GetRightNode()
 {
     return this->right;
