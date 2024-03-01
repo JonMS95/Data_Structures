ff64dca8a20b14d9d7e19a88f8164f6c2dc2e4b2
JonMS95 <jon_portugalete@hotmail.com>
2024-03-01 17:07:45 +0100

    "insertStringInTrie" method has been slighlty modified due to the modifications in the way pending nodes are stored for each trie node.

--- Trie/Source_files/Trie.cpp
+++ Trie/Source_files/Trie.cpp
@@ -1,6 +1,6 @@
 #include "Trie.hpp"
 
-Trie::Trie(const bool is_case_sensitive): root(std::make_shared<TrieNode>('\0')), case_sensitive(is_case_sensitive)
+Trie::Trie(const bool is_case_sensitive): root(std::make_shared<TrieNode>('\0', is_case_sensitive))
 {
     std::cout << "Created new trie. Case sensitivity: " << (is_case_sensitive ? "en" : "dis") << "abled." << std::endl;
 }
@@ -10,21 +10,22 @@ Trie::~Trie(void)
     std::cout << "Destroyed trie." << std::endl;
 }
 
+Trie::Trie(Trie& t): root(t.root) {}
+
 void Trie::insertStringInTrie(  const std::string& s                    ,
                                 std::shared_ptr<TrieNode> current_node  ,
                                 long unsigned int current_index         )
 {
-    if(current_index >= s.size())
+    if(current_index >= s.size() || current_node == nullptr)
         return;
-    
-    std::shared_ptr<TrieNode> tn = current_node->getPendingNode(s[current_index], this->case_sensitive);
+
+    std::shared_ptr<TrieNode> tn = current_node->getPendingLetter(s[current_index]);
 
     if(tn == nullptr)
-    {
         current_node->addPendingNode(s[current_index]);
-        tn = current_node->getPendingNode(s[current_index], this->case_sensitive);
-    }
     
+    tn = current_node->getPendingLetter(s[current_index]);
+
     ++current_index;
     this->insertStringInTrie(s, tn, current_index);
 }
