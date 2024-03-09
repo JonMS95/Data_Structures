80f6daebd1b4a0f7d7cc6771e5da0c3a1b319457
JonMS95 <jon_portugalete@hotmail.com>
2024-03-09 12:42:03 +0100

    Added copy constructor for trie.

--- Trie/Source_files/Trie.cpp
+++ Trie/Source_files/Trie.cpp
@@ -2,15 +2,18 @@
 
 Trie::Trie(const bool is_case_sensitive): root(std::make_shared<TrieNode>('\0', is_case_sensitive))
 {
-    std::cout << "Created new trie. Case sensitivity: " << (is_case_sensitive ? "en" : "dis") << "abled." << std::endl;
+    std::cout << "Created new trie with root at: " << this->root << ". Case sensitivity: " << (is_case_sensitive ? "en" : "dis") << "abled." << std::endl;
+}
+
+Trie::Trie(const Trie& t): root(std::make_shared<TrieNode>(*(t.root)))
+{
+    std::cout << "Created Trie with root at: " << this->root << "by copying it from root at: " << t.root << "." << std::endl;
 }
 
 Trie::~Trie(void)
 {
     std::cout << "Destroyed trie." << std::endl;
 }
-
-Trie::Trie(Trie& t): root(t.root) {}
 
 void Trie::insertStringInTrie(  const std::string& s                    ,
                                 std::shared_ptr<TrieNode> current_node  ,
