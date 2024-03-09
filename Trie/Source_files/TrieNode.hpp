e49686d2ad06db55ba160951aa27d0a5c2f5ee1f
JonMS95 <jon_portugalete@hotmail.com>
2024-03-09 12:41:03 +0100

    Added copy constructor for trie node.

--- Trie/Source_files/TrieNode.hpp
+++ Trie/Source_files/TrieNode.hpp
@@ -14,15 +14,14 @@ private:
     const char letter;
     const bool case_sensitive;
     std::map<char, std::shared_ptr<TrieNode>> pending_letters;
-    
     std::string getPendingNodesAsString(void);
 
 public:
     TrieNode(void) = delete;
     TrieNode(char c, bool case_sensitive = false);
     TrieNode(char c, std::vector<char> pending_letters, bool case_sensitive = false);
+    TrieNode(const TrieNode& other);
     virtual ~TrieNode(void);
-    TrieNode(const TrieNode& tn);
 
     char getTrieNodeLetter(void);
     void addPendingNode(char c);
