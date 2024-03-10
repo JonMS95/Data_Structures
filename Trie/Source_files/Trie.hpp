58fc9e57c32947c8957346ecd9d8aebc9ce91526
JonMS95 <jon_portugalete@hotmail.com>
2024-03-10 16:16:57 +0100

    Added a function to get the last node from a starting string. Added as well another function to get all the strings registered in a trie as a vector of strings.

--- Trie/Source_files/Trie.hpp
+++ Trie/Source_files/Trie.hpp
@@ -9,11 +9,21 @@
 class Trie
 {
 private:
+public: // move it to private
     std::shared_ptr<TrieNode> root;
 
+private:
     void insertStringInTrie(const std::string& s                    ,
                             std::shared_ptr<TrieNode> current_node  ,
                             long unsigned current_index = 0         );
+    void getAllStringsInTrie(   std::shared_ptr<TrieNode> current_node      ,
+                                std::string& current_string                 ,
+                                std::vector<std::string>& strings_in_trie   );
+public: // move it to private
+    std::shared_ptr<TrieNode> getlastNodeFromStartingString(  std::shared_ptr<TrieNode> current_node  ,
+                                                                    const std::string& starting_string      ,
+                                                                    unsigned int current_index = 0          );
+    // std::vector<std::string> Trie::getAllStringsFromStartingNode(std::shared_ptr<TrieNode> current_node, std::string& starting_string, unsigned int current_index = 0);
 
 public:
     Trie(const bool is_case_sensitive = true);
@@ -21,6 +31,8 @@ public:
     virtual ~Trie(void);
 
     void insertString(const std::string s);
+    // std::vector<std::string> getAllStringsFromStartingString(const std::string& starting_string);
+    std::vector<std::string> getAllStrings(void);
 };
 
 #endif
