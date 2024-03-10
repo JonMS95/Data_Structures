cd0a357454c97a83fbe97db8c8522d3f1f5b369a
JonMS95 <jon_portugalete@hotmail.com>
2024-03-10 22:40:32 +0100

    getAllStringsFromStartingString is now the ruling function. getAllStringsInTrie is a particular case of the prior function indeed.

--- Trie/Source_files/Trie.hpp
+++ Trie/Source_files/Trie.hpp
@@ -9,21 +9,19 @@
 class Trie
 {
 private:
-public: // move it to private
     std::shared_ptr<TrieNode> root;
 
-private:
     void insertStringInTrie(const std::string& s                    ,
                             std::shared_ptr<TrieNode> current_node  ,
                             long unsigned current_index = 0         );
-    void getAllStringsInTrie(   std::shared_ptr<TrieNode> current_node      ,
-                                std::string& current_string                 ,
-                                std::vector<std::string>& strings_in_trie   );
-public: // move it to private
-    std::shared_ptr<TrieNode> getlastNodeFromStartingString(  std::shared_ptr<TrieNode> current_node  ,
-                                                                    const std::string& starting_string      ,
-                                                                    unsigned int current_index = 0          );
-    // std::vector<std::string> Trie::getAllStringsFromStartingNode(std::shared_ptr<TrieNode> current_node, std::string& starting_string, unsigned int current_index = 0);
+
+    std::shared_ptr<TrieNode> getlastNodeFromStartingString(std::shared_ptr<TrieNode> current_node  ,
+                                                            const std::string& starting_string      ,
+                                                            unsigned int current_index = 0          );
+
+    void getAllStringsFromStartingNode( std::shared_ptr<TrieNode> current_node      ,
+                                        std::string& current_string                 ,
+                                        std::vector<std::string>& strings_in_trie   );
 
 public:
     Trie(const bool is_case_sensitive = true);
@@ -31,8 +29,8 @@ public:
     virtual ~Trie(void);
 
     void insertString(const std::string s);
-    // std::vector<std::string> getAllStringsFromStartingString(const std::string& starting_string);
-    std::vector<std::string> getAllStrings(void);
+    std::vector<std::string> getAllStringsInTrie(void);
+    std::vector<std::string> getAllStringsFromStartingString(std::string starting_string = "");
 };
 
 #endif
