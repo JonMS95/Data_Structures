8e784a7d1c2be95a971e3de7df516bcd12a99cb8
JonMS95 <jon_portugalete@hotmail.com>
2024-03-15 17:39:09 +0100

    Embedded all the test cases in new test functions.

--- Trie/Source_files/main.cpp
+++ Trie/Source_files/main.cpp
@@ -1,34 +1,55 @@
 #include "Trie.hpp"
+#include <vector>
+#include <string>
+
+void testAddStringsToTrie(Trie& t, std::vector<std::string>& strings_to_add)
+{
+    for(std::string s:strings_to_add)
+        t.insertString(s);
+}
+
+void testSearchStringsFromStartingString(Trie& t, std::vector<std::string>& strings_to_search)
+{
+    for(std::string string_to_search:strings_to_search)
+    {
+        std::cout << std::endl << "******* t2.getAllStringsFromStartingString(\"" << string_to_search << "\") *******" << std::endl;
+        for(std::string s : t.getAllStringsFromStartingString(string_to_search))
+            std::cout << s << std::endl;
+    }
+}
 
 int main()
 {
+    std::vector<std::string> strings_to_add =
+    {
+        "and"    ,
+        "ant"    ,
+        "dad"    ,
+        "do"     ,
+        "dance"  ,
+        "dank"   ,
+        "dank"   
+    };
+
+    std::vector<std::string> strings_to_search = 
+    {
+        "and"   ,   // This one does exist in the Trie, but no other string starts with those characters.
+        "da"    ,
+        "ande"      // Last one does not exist in the Trie, so it should return nothing.
+    };
+
     Trie t(false);
-    t.insertString("and"    );
-    t.insertString("ant"    );
-    t.insertString("dad"    );
-    t.insertString("do"     );
-    t.insertString("dance"  );
-    t.insertString("dank"   );
-    t.insertString("dank"   );
 
-    Trie t2(t);
+    testAddStringsToTrie(t, strings_to_add);
 
-    std::string test_string_1 = "and";
-    std::cout << std::endl << "******* t2.getAllStringsFromStartingString(\"" << test_string_1 << "\") *******" << std::endl;
-    for(std::string s : t2.getAllStringsFromStartingString(test_string_1))
-        std::cout << s << std::endl;
-    
-    std::string test_string_2 = "da";
-    std::cout << std::endl << "******* t2.getAllStringsFromStartingString(\"" << test_string_2 << "\") *******" << std::endl;
-    for(std::string s : t2.getAllStringsFromStartingString(test_string_2))
-        std::cout << s << std::endl;
-    
-    std::string test_string_3 = "ande";
-    std::cout << std::endl << "******* t2.getAllStringsFromStartingString(\"" << test_string_3 << "\") *******" << std::endl;
-        for(std::string s : t2.getAllStringsFromStartingString(test_string_3))
-            std::cout << s << std::endl;
+    Trie t2 = t;
+
+    t.clear();
+
+    testSearchStringsFromStartingString(t2, strings_to_search);
     
     std::cout << std::endl << "******* t2.getAllStringsInTrie() *******" << std::endl;
     for(std::string s : t2.getAllStringsInTrie())
         std::cout << s << std::endl;
+    std::cout << std::endl;
 }
